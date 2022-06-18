eda03c0fc6897725d51d301aef71af39a063b538
Jessica Grosskopf <jgrossp@gmail.com>
2022-06-18 19:06:13 -0300

    Criando js de criação de aula

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,17 @@
+/* var nome = 'Jessica';
+var idade = 35;
+console.log(`O meu nome é ${nome} e tenho idade  de ${idade}`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Cuiabá`);
+console.log(listaDeDestinos);
+ listaDeDestinos.push(`Curitiba`);
+ console.log(listaDeDestinos);
+ */
+
+ console.error('deu erro')
